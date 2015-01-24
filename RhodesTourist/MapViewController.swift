5d7509b8288e24adab93097c449a9ce7a626370b
miniroo321 <miniroo321@gmail.com>
2015-01-23 20:47:11 -0600

    Added swift file

---
+++ RhodesTourist/MapViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MapViewController.swift
+//  RhodesTourist
+//
+//  Created by Will Cobb on 1/21/15.
+//  Copyright (c) 2015 Apprentice Media LLC. All rights reserved.
+//
+
+import Foundation
